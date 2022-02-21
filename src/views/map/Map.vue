01467737e8ce7c188b617dd8b8f2a4840e3ed2a6
Youzege <youzegehq@gmail.com>
2022-02-21 20:37:41 +0800

    build: build

--- src/views/map/Map.vue
+++ src/views/map/Map.vue
@@ -1,25 +1,33 @@
 <template>
   <div class="map-container">
     <ze-map>
-      <ze-view :center="center" :zoom="12" ></ze-view>
+      <ze-view :center="center" :zoom="12"></ze-view>
 
       <ze-tile-layer>
-        <ze-source-XYZ :url="baseUrl" :attributions="attributions"></ze-source-XYZ>
+        <ze-source-XYZ
+          :url="baseUrl"
+          :attributions="attributions"
+        ></ze-source-XYZ>
       </ze-tile-layer>
       <ze-tile-layer>
-        <ze-source-XYZ :url="noteUrl" :attributions="attributions"></ze-source-XYZ>
+        <ze-source-XYZ
+          :url="noteUrl"
+          :attributions="attributions"
+        ></ze-source-XYZ>
       </ze-tile-layer>
     </ze-map>
   </div>
 </template>
 
 <script setup>
-import { } from 'vue';
+import {} from 'vue'
 
 const center = [113.27, 23.1]
 
-const baseUrl = 'http://t0.tianditu.com/DataServer?T=vec_w&x={x}&y={y}&l={z}&tk=40e50cca6823476482e8721bb2eee900'
-const noteUrl = 'http://t0.tianditu.com/DataServer?T=cva_w&x={x}&y={y}&l={z}&tk=40e50cca6823476482e8721bb2eee900'
+const baseUrl =
+  'https://t0.tianditu.com/DataServer?T=vec_w&x={x}&y={y}&l={z}&tk=40e50cca6823476482e8721bb2eee900'
+const noteUrl =
+  'https://t0.tianditu.com/DataServer?T=cva_w&x={x}&y={y}&l={z}&tk=40e50cca6823476482e8721bb2eee900'
 
 const attributions = '天地图XYZ'
 </script>
