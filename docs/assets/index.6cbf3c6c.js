6a821f6cdf7240842921c9d6b16834297112e15f
Youzege <youzegehq@gmail.com>
2022-02-22 19:43:24 +0800

    build: build

--- docs/assets/index.6cbf3c6c.js
+++
@@ -1,1 +0,0 @@
-import{o as e,a as o}from"./vendor.fcb96a64.js";const t={setup(r){return(a,c)=>(e(),o("div",null,"\u8FD9\u91CC\u8FD8\u672A\u5F00\u53D1..."))}};var p=t;export{p as default};
---
+++ docs/assets/index.9e7f6244.js
@@ -0,0 +1,1 @@
+import{o as e,a as o}from"./vendor.624c609b.js";const t={setup(r){return(a,c)=>(e(),o("div",null,"\u53F0\u98CE"))}};var p=t;export{p as default};
