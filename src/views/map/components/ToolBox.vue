988e6e9121efd12559cf62057f88a2288efe1d88
Youzege <youzegehq@gmail.com>
2022-02-23 22:26:53 +0800

    feat: 工具箱开发准备

---
+++ src/views/map/components/ToolBox.vue
@@ -0,0 +1,18 @@
+<template>
+  <el-dropdown>
+    <el-button color="#626aef" :icon="Briefcase" plain> 工具箱 </el-button>
+    <template #dropdown>
+      <el-dropdown-menu>
+        <el-dropdown-item>测量距离</el-dropdown-item>
+        <el-dropdown-item>清除测量</el-dropdown-item>
+      </el-dropdown-menu>
+    </template>
+  </el-dropdown>
+</template>
+
+<script setup>
+import {} from 'vue'
+import { Briefcase } from '@element-plus/icons-vue'
+</script>
+
+<style lang="scss" scoped></style>
